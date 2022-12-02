b2d4dab90326e381510e9ef1d8319d06cf24f20f
fmher <feumatthewher2@gmail.com>
2022-12-02 11:42:17 -0800

    made start button work, changed score = 0,score to beat = 9999

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const clickBox1 = document.querySelector('.click-box1')
-const score = document.querySelector('#score-holder')
-const btn = document.querySelector('.click-box1')
+let score = document.querySelector('#score-holder')
+const scoreToBeat = document.querySelector('#score-to-beat')
+const targetBtn = document.querySelector('.click-box1')
+const startBtn = document.querySelector('#start')
 
 // console.log(score)
 // console.log(btn)
 
-
-btn.addEventListener('click', () => {
-    let scoreUp = score.innerText = 10
-    console.log('been clicked!')
-    btn.innerText = 'got clicked!'
-    scoreUp += 100
+startBtn.addEventListener('click', () => {
+    score.innerText = 0
+    scoreToBeat.innerText = 9999
 
 })
+
+
+targetBtn.addEventListener('click', () => {
+    // score = score.innerText = 0
+    console.log('new score', score)
+    // console.log('score =', score)
+    // console.log('been clicked!')
+    targetBtn.innerText = 'got clicked!'
+    
+    // doesnt work idk
+    // suppose increment score up by 10 each time btn is clicked
+    score = scoreUp(0)
+
+})
+
+
+const scoreUp = (num) => {
+    num+=10
+    return num
+}
+// console.log(scoreUp(10))
+// console.log(scoreUp(score))
+
+
+
