1e0a540f1967e090f8c642b335043cfdcefcf3d8
fmher <feumatthewher2@gmail.com>
2022-12-02 12:36:33 -0800

    every box works when start button has been clicked

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
-const clickBox1 = document.querySelector('.click-box1')
+const startBtn = document.querySelector('#start')
 let score = document.querySelector('#score-holder')
 const scoreToBeat = document.querySelector('#score-to-beat')
-const targetBtn = document.querySelector('.click-box1')
-const startBtn = document.querySelector('#start')
+const clickBox1 = document.querySelector('#box1')
+const clickbox2 = document.querySelector('#box2')
+const clickbox3 = document.querySelector('#box3')
+const clickbox4 = document.querySelector('#box4')
+const clickbox5 = document.querySelector('#box5')
+const clickbox6 = document.querySelector('#box6')
 
 // console.log(score)
 // console.log(btn)
 
+
+// changes score and score to beat to a number
+// eventually will add a timer when start button is pressed
 startBtn.addEventListener('click', () => {
     score.innerText = 0
     scoreToBeat.innerText = 9999
 
-})
-
-
-targetBtn.addEventListener('click', () => {
-   score.innerText = scoreUp(+score.innerText)
-    console.log('new score', score)
-    // console.log('score =', score)
-    // console.log('been clicked!')
-    targetBtn.innerText = 'got clicked!'
+    clickBox1.addEventListener('click', () => {
+       score.innerText = scoreUp(+score.innerText)
+        // console.log('new score', score)
+        // console.log('score =', score)
+        // console.log('been clicked!')
+        
+        
+        // doesnt work idk
+        // suppose increment score up by 10 each time btn is clicked
+        scoreUp(+score.innerText)
     
-    // doesnt work idk
-    // suppose increment score up by 10 each time btn is clicked
-    scoreUp(+score.innerText)
+    })
 
+    clickbox2.addEventListener('click', () => {
+        score.innerText = scoreUp(+score.innerText)
+        scoreUp(+score.innerText)
+    })
+
+    clickbox3.addEventListener('click', () => {
+        score.innerText = scoreUp(+score.innerText)
+        scoreUp(+score.innerText)
+    })
+
+    clickbox4.addEventListener('click', () => {
+        score.innerText = scoreUp(+score.innerText)
+        scoreUp(+score.innerText)
+    })
+
+    clickbox5.addEventListener('click', () => {
+        score.innerText = scoreUp(+score.innerText)
+        scoreUp(+score.innerText)
+    })
+
+    clickbox6.addEventListener('click', () => {
+        score.innerText = scoreUp(+score.innerText)
+        scoreUp(+score.innerText)
+    })
 })
 
-
+//adds the score up, increments
 const scoreUp = (num) => {
     num+=10
     return num
 }
 
-// score.innerText = '10'
 
+
+
+
+
+// score.innerText = '10'
 // console.log(scoreUp(10))
 // // by adding + infront, converts string into a number
 // console.log(scoreUp(+score.innerText))
