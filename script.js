69702b454c1780acda03ef0d7217ad840f690b0c
fmher <feumatthewher2@gmail.com>
2022-12-02 10:50:01 -0800

    got js to work

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const clickBox1 = document.querySelector('.click-box1')
-const score = document.querySelector('#score-holder').innerText
-console.log(score)
+const score = document.querySelector('#score-holder')
+const btn = document.querySelector('.click-box1')
+
+// console.log(score)
+// console.log(btn)
 
 
+btn.addEventListener('click', () => {
+    let scoreUp = score.innerText = 10
+    console.log('been clicked!')
+    btn.innerText = 'got clicked!'
+    scoreUp += 100
 
-clickBox1.addEventListener('click', () => {
-    score.text = '100'
 })
-
