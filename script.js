2f99c12eb3780d81a17503938f8115b1a704604c
fmher <feumatthewher2@gmail.com>
2022-12-02 16:18:59 -0800

    added pop up when you win! reset work but adds +10 to increment

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const clickbox3 = document.querySelector('#box3')
 const clickbox4 = document.querySelector('#box4')
 const clickbox5 = document.querySelector('#box5')
 const clickbox6 = document.querySelector('#box6')
+const pTag = document.querySelector('#p')
 // checks to see if I targeted the right element correctly
-// console.log( resetBtn)
+// console.log( popUp)
 
 
 // changes score and score to beat to a number
@@ -21,9 +22,10 @@ startBtn.addEventListener('click', () => {
     
     // creates random number that you will have to beat!
     let randomNum = Math.round((Math.random() * 10000) * 0.001) * 10
-    // console.log(randomNum)
+    console.log(randomNum)
     scoreToBeat.innerText = randomNum
-
+    console.log(scoreToBeat)
+    console.log()
     render()
     
 })
@@ -33,20 +35,17 @@ const render = () => {
 
     clickBox1.addEventListener('click', () => {
        score.innerText = scoreUp(+score.innerText)
-        // console.log('new score', score)
-        // console.log('score =', score)
-        // console.log('been clicked!')
-        
-        
+      
+       // win condition works, but if pressed to fast you break game, idk
+       // goes up by 10!!
         scoreUp(+score.innerText)
         
-        // win condition works, but if pressed to fast you break game, idk
-        // doesnt work idk, ----> NOW DOES WORK = IDK
-        // suppose increment score up by 10 each time btn is clicked
-        // start button counts score incrementing up
-        //EX score to beat 10, click start 2 times then target after = win
+        // creates a pop telling you that you won!
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
         } 
     
     })
@@ -56,7 +55,11 @@ const render = () => {
         scoreUp(+score.innerText)
     
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
+
         } 
     })
     
@@ -65,7 +68,10 @@ const render = () => {
         scoreUp(+score.innerText)
     
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
         } 
     })
     
@@ -74,7 +80,10 @@ const render = () => {
         scoreUp(+score.innerText)
     
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
         } 
     })
     
@@ -83,7 +92,10 @@ const render = () => {
         scoreUp(+score.innerText)
     
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
         } 
     })
     
@@ -92,7 +104,10 @@ const render = () => {
         scoreUp(+score.innerText)
     
         if (score.innerText > scoreToBeat.innerText) {
-            grid.innerText = 'you won!'
+            const popUp = document.createElement('p')
+            const youWin = document.createTextNode('you win!')
+            popUp.appendChild(youWin)
+            pTag.appendChild(popUp)
         } 
     
     })
@@ -111,17 +126,15 @@ resetBtn.addEventListener('click', () => {
     console.log('reset button pressed!')
 
     //resets score and score to beat back to normal
-    console.log('holding score', score.innerText)
-    score.innerText = null
+    // console.log('holding score', score.innerText)
+    score.innerText = 'score'
     scoreToBeat.innerText = 'score to beat!'
     
-    // trying to convert you win back to targets, but unable too
-    // grid.innerText = grid2.innerText('.boxstyle')
-    // render()
-    console.log(grid)
+    pTag.innerText = null
+    
 })
 
-console.log(1)
+
 
 // score.innerText = '10'
 // console.log(scoreUp(10))
